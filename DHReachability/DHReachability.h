69fac46dd1214d465ddb3ec3a74100b92d660b6b
David Hardiman <david_hardiman@yahoo.co.uk>
2017-04-04 12:47:16 +0100

    Add DHReachability framework target

---
+++ DHReachability/DHReachability.h
@@ -0,0 +1,19 @@
+//
+//  DHReachability.h
+//  DHReachability
+//
+//  Created by David Hardiman on 04/04/2017.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DHReachability.
+FOUNDATION_EXPORT double DHReachabilityVersionNumber;
+
+//! Project version string for DHReachability.
+FOUNDATION_EXPORT const unsigned char DHReachabilityVersionString[];
+
+#import "DHReachabilityEventHandler.h"
+
+
